68e5e0ba2649752d5d1bdf4785a999a4c31df428
flamekong <17229268+flamekong@users.noreply.github.com>
2018-02-11 18:20:57 +1030

    A ping command that actually checks latency

--- commands/info.py
+++ commands/info.py
@@ -8,7 +8,15 @@ class Info:
 
     @commands.command()
     async def ping(self, ctx):
-        await ctx.send("Pong!")
+        t1 = time.perf_counter()
+        await ctx.channel.trigger_typing()
+        t2 = time.perf_counter()
+        # Singles quotes to match my relationship status
+        fmt = '\U0001f3d3 **Pong!** `{}ms`'.format(str(round((t2 - t1) * 100)))
+        # An embed for 7 letters, yes
+        em = discord.Embed(description=fmt,
+                            color=0xff3f3f)
+        await ctx.send(embed=em)
 
     @commands.command()
     async def help(self, ctx):
