6dacf4cce8f5625291f40ddafdb54ee43537a1ed
flamekong <17229268+flamekong@users.noreply.github.com>
2018-02-11 18:24:38 +1030

    wrong place
     added the new ping command to the wrong cog

--- commands/info.py
+++ commands/info.py
@@ -6,17 +6,6 @@ class Info:
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    async def ping(self, ctx):
-        t1 = time.perf_counter()
-        await ctx.channel.trigger_typing()
-        t2 = time.perf_counter()
-        # Singles quotes to match my relationship status
-        fmt = '\U0001f3d3 **Pong!** `{}ms`'.format(str(round((t2 - t1) * 100)))
-        # An embed for 7 letters, yes
-        em = discord.Embed(description=fmt,
-                            color=0xff3f3f)
-        await ctx.send(embed=em)
 
     @commands.command()
     async def help(self, ctx):
