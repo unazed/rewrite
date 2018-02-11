51425f0a2f342ada59bfcbb50408acff5cfe0a7a
flamekong <17229268+flamekong@users.noreply.github.com>
2018-02-11 18:26:43 +1030

    pingo commando

--- commands/ping.py
+++ commands/ping.py
@@ -1,12 +1,19 @@
 from discord.ext import commands
-
+import time
 
 class PingCommand:
 
     @commands.command()
     async def ping(self, ctx):
-        print(ctx)
-        await ctx.send("Pong!")
+        t1 = time.perf_counter()
+        await ctx.channel.trigger_typing()
+        t2 = time.perf_counter()
+        # Singles quotes to match my relationship status
+        fmt = '\U0001f3d3 **Pong!** `{}ms`'.format(str(round((t2 - t1) * 100)))
+        # An embed for 7 letters, yes
+        em = discord.Embed(description=fmt,
+                            color=0xff3f3f)
+        await ctx.send(embed=em)
 
 
 def setup(bot):
