e3777c6917d4b2db37fd762c46a98f576d14c3c8
repyh00 <34779541+repyh00@users.noreply.github.com>
2018-02-20 17:06:56 -0500

    Fixed eval command
    Probably doesn't work (wasn't able to test it).

--- commands/eval.py
+++ commands/eval.py
@@ -1,59 +1,51 @@
+from contextlib import redirect_stdout
+from discord.ext import commands
+from utils.misc import (cleanup_code, get_syntax_error)
 import inspect
 import io
-import textwrap
 import traceback
-from contextlib import redirect_stdout
-
-from discord.ext import commands
-
-from utils.visual import SUCCESS
-from utils.misc import cleanup_code, get_syntax_error
 
 
-class Eval:
-
+class Eval(object):
     def __init__(self, bot):
         self.bot = bot
-        self._last_result = None
-        self.sessions = set()
-
+        self.context = { "bot": self.bot, "phm": self.bot.player_handler_manager, "lava": self.bot.lavalink }
+        self.last_result = None
+    
     @commands.command(aliases=["e"])
     @commands.is_owner()
-    async def eval(self, ctx, *, body: str):
-        env = {
-            "bot": self.bot,
-            "phm": self.bot.player_handler_manager,
-            "lava": self.bot.lavalink,
+    async def eval(self, ctx, *, code: str):
+        code = cleanup_code(code)
+        stdout = io.StringIO()
+        result = ""
+
+        self.context.update({
             "ctx": ctx,
             "ch": ctx.channel,
             "author": ctx.author,
             "srv": ctx.guild,
             "msg": ctx.message,
-            "_": self._last_result,
-        }
-        env.update(globals())
-        body = cleanup_code(body)
-        stdout = io.StringIO()
-        executor = eval
-        try:
-            code = compile(body, "<debug session>", "eval")
-        except SyntaxError:
-            executor = exec
-            try:
-                code = compile(body, '<repl session>', 'exec')
-            except SyntaxError as e:
-                await ctx.send(get_syntax_error(e))
-                return
+            "_": self.last_result
+        })
+
         try:
             with redirect_stdout(stdout):
-                result = executor(code, env)
-                if inspect.isawaitable(result):
-                    result = await result
-                self._last_result = result
+                for executor in (eval, exec):
+                    try:
+                        result = executor(code, self.context)
+                        if inspect.isawaitable(result):
+                            result = await result
+                        break
+                    except SyntaxError as e:
+                        result = get_syntax_error(e)
         except:
-            value = stdout.getvalue()
-            await ctx.send("```py\n{}{}\n```".format(value, traceback.format_exc()))
-            return
+            result = stdout.getvalue() + traceback.format_exc()
+        finally:
+            if not result:
+                result = stdout.getvalue()
+            
+            self.last_result = result
+        
         await ctx.send(f"```py\n{result}\n```")
 
 
